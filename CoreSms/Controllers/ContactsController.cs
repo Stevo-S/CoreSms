801ead0e55be63048cdc47882708374823e17d1f
Stevo-S <stevesavio@rocketmail.com>
2020-09-01 17:13:53 +0300

    Add contacts controller

---
+++ CoreSms/Controllers/ContactsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoreSms.Data;
+using CoreSms.Models;
+
+namespace CoreSms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(long id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // PUT: api/Contacts/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(long id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            contact.UpdatedAt = DateTime.Now;
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contacts
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            contact.CreatedAt = DateTime.Now;
+            contact.UpdatedAt = DateTime.Now;
+
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostContact([FromBody] Contact[] contacts)
+        {
+            foreach(var contact in contacts)
+            {
+                contact.CreatedAt = DateTime.Now;
+                contact.UpdatedAt = DateTime.Now;
+
+                _context.Contacts.Add(contact);
+            }
+            await _context.SaveChangesAsync();
+
+            return Ok("Contacts inserted successfully");
+        }
+
+        // DELETE: api/Contacts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Contact>> DeleteContact(long id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return contact;
+        }
+
+
+        private bool ContactExists(long id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
