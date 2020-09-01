474d4d1f99e2f24a664d308d8a3492c701aa3a3e
Stevo-S <stevesavio@rocketmail.com>
2020-09-01 08:07:52 +0300

    Add outgoing messages

---
+++ CoreSms/Models/OutboundMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSms.Models
+{
+    public class OutboundMessage
+    {
+        public long Id { get; set; }
+        public string BulkId { get; set; }
+        public string Sender { get; set; }
+        public string Destination { get; set; }
+        public string Content { get; set; }
+        public DateTime? SentAt { get; set; }
+        public DateTime? DoneAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
